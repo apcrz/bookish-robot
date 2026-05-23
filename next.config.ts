5a53cd8c7ca5672032f5bf170d4602183b149939
Allan Possani <alpcrz@gmail.com>
2026-05-23 15:26:30 -0300

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa"
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    skipWaiting: true 
+  }
+});
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   allowedDevOrigins: ['192.168.237.69', '192.168.237.91'],
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
